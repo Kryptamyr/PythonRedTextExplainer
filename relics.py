dfd73ce29579b1da0c2fb37b5c6eebae3fbe2790
Corbino <corbin.gossett2204@gmail.com>
2025-09-23 23:35:52 -0500

    relics list

---
+++ relics.py
@@ -0,0 +1,6 @@
+# Relic red text explanations
+# Note: Most relics in Borderlands 2 don't have red text, but some special ones might
+RELIC_COMMANDS = [
+    # Add relic commands here if any are found
+    # Most relics are passive and don't have red text descriptions
+]
